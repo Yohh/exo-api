f7d3abc54522cc220b2c9af8e85654260bf46738
Yohh <durandyohan@zaclys.net>
2022-06-08 18:33:32 +0200

    react finished

--- api_react/src/App.js
+++ api_react/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  let [string, setString] = useState();
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((resp) => resp.json())
+      .then((data) => {
+        string = data.value;
+      });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="text">{string}</p>
+      <button type="button" className="update" onClick={() => setString(string)}>
+        update
+      </button>
     </div>
   );
 }
